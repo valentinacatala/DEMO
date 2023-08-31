28aba1a49f78218a520d29456a8a00cec5a0b372
Alumnos <Alumnos@PC179>
2023-08-31 10:22:00 -0300

    Agregar archivos de proyecto.

---
+++ DEMO/PROBLEMA.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DEMO
+{
+    class PROBLEMA
+    {
+        public void saludar()
+        {
+            MessageBox.Show("HOLA");
+        }
+        public void verFrutas(ListBox lst)
+        {
+            string[] frutas = { "uva", "pera", "limon","sandia", "manzana" };
+            lst.Items.Clear();
+            foreach (string fruta in frutas)
+            {
+                lst.Items.Add(fruta);
+            }
+        }
+
+    }
+}
